c9ce7dbaae2396f85e5edc6e28195fda56a7b4df
SapanSidhwani <sapansidhwani40@gmail.com>
2024-10-06 10:30:17 +0530

    Routing

---
+++ resources/views/sound.blade.php
@@ -0,0 +1,7 @@
+<h1>Hello, {{$name}}</h1>
+
+<!-- http://127.0.0.1:8000/about -->
+<a href="{{url('/about')}}">Goto About Page</a>
+
+<!-- /about -->
+<a href='/about'>Goto About Page</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Dynamic routing
+Route::get('/sound/{name}', function ($name) {
+    return view('sound', ['name' => $name]);
+});
+
+// For redirection
+// Route::redirect('/', '/home');
+
+// For view
+// Route::view('/', 'home');
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+// Route::get($uri, $callback);
+// Route::post($uri, $callback);
+// Route::put($uri, $callback);
+// Route::patch($uri, $callback);
+// Route::delete($uri, $callback);
+// Route::options($uri, $callback);
